0d63e0bdcc8f635c2e4f8fee7e1cbdc4ec9f87fa
fedorkolmykow <fedorkolmykow@gmail.com>
2020-09-14 15:06:38 +0300

    add expire time

--- jobber/pkg/redis/redis.go
+++ jobber/pkg/redis/redis.go
@@ -9,16 +9,16 @@ import (
 )
 
 type DbClient interface {
-    Get(key string) (value string, err error)
-    Set(key string, value string) (err error)
-    Delete(key string) (err error)
+    Get(key interface{}) (value string, err error)
+    Set(key interface{}, value string) (err error)
+    Delete(key interface{}) (err error)
 }
 
 type db struct {
 	pool *redis.Pool
 }
 
-func (d *db) Get(key string) (value string, err error){
+func (d *db) Get(key interface{}) (value string, err error){
 	conn := d.pool.Get()
 	defer conn.Close()
 	value, err = redis.String(conn.Do("GET", key))
@@ -31,17 +31,21 @@ func (d *db) Get(key string) (value string, err error){
 	return
 }
 
-func (d *db) Set(key string, value string) (err error){
+func (d *db) Set(key interface{}, value string) (err error){
 	conn := d.pool.Get()
 	defer conn.Close()
 	_, err = conn.Do("SET", key, value)
 	if err != nil {
 		return
 	}
+	_, err = conn.Do("EXPIRE", key, os.Getenv("CASH_EXPIRE"))
+	if err != nil {
+		return
+	}
 	return
 }
 
-func (d *db) Delete(key string) (err error){
+func (d *db) Delete(key interface{}) (err error){
 	conn := d.pool.Get()
 	defer conn.Close()
 	_, err = conn.Do("DEL", key)
@@ -57,7 +61,7 @@ func NewDb() DbClient {
 		MaxIdle:     5,
 		IdleTimeout: 240 * time.Second,
 		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", os.Getenv("DATABASE_URL"))
+			return redis.Dial("tcp", os.Getenv("REDIS_URL"))
 		},
 	}
 
