02fe1a02b7bbf039b2aa9baf05ccd3d58120dd65
fedorkolmykow <fedorkolmykow@gmail.com>
2020-09-14 18:49:58 +0300

    pagination tests

---
+++ jobber/pkg/service/service_test.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	m "github.com/fedorkolmykow/avitojob/pkg/models"
+	"reflect"
+	"testing"
+)
+
+func testDummy(){
+
+}
+
+func TestPaginationSuccess(t *testing.T){
+	perPage := 3
+	page := 1
+	l := 5
+	trs := make([]m.Transaction, l)
+	for i := 0; i < l; i++{
+		trs[i].UserId = i
+	}
+	tr := pagination(trs, page, perPage)
+	for i := 0; i < perPage; i++{
+		if !reflect.DeepEqual(tr[i], trs[(page - 1) * perPage+ i]){
+			t.Errorf("wrong pagination:\n%+v\n%+v\n", tr, trs)
+		}
+	}
+}
+
+func TestPaginationShortPage(t *testing.T){
+	perPage := 3
+	page := 2
+	l := 5
+	trs := make([]m.Transaction, l)
+	for i := 0; i < l; i++{
+		trs[i].UserId = i
+	}
+	tr := pagination(trs, page, perPage)
+	for i := 0; i < len(tr); i++{
+		if !reflect.DeepEqual(tr[i], trs[(page - 1) * perPage+ i]){
+			t.Errorf("wrong pagination:\n%+v\n%+v\n", tr, trs)
+		}
+	}
+}
+
+func TestPaginationLastPage(t *testing.T){
+	perPage := 3
+	page := 999
+	l := 6
+	trs := make([]m.Transaction, l)
+	for i := 0; i < l; i++{
+		trs[i].UserId = i
+	}
+	tr := pagination(trs, page, perPage)
+	for i := 0; i < len(tr); i++{
+		if !reflect.DeepEqual(tr[i], trs[3 + i]){
+			t.Errorf("wrong pagination:\n%+v\n%+v\n", tr, trs)
+		}
+	}
+}
